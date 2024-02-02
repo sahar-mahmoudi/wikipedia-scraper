247fafd004d8accf7072b90135996eae4e9ad328
Sahar Mahmoudi <sahar@Sahars-MacBook-Pro.local>
2024-02-02 14:11:19 +0100

    Must-have features

--- main.py
+++ main.py
@@ -1,111 +1,3 @@
-# import requests
-# import json
-# from bs4 import BeautifulSoup
-# import re
-# import time
-
-
-
-
-
-# start_time = time.time()
-# s = requests.Session() 
-
-# def refresh_cookie(root_url):
-#     response = requests.get(root_url + "cookie/")
-#     return response.cookies
-
-
-
-# def clean_par(first_paragraph): # Define a regular expression pattern to match references and HTML tags
-#     pattern =r'\([^)]*uitspraakⓘ[^)]*\)|\[\d+\]|\n' + f'|{r"//w+?;"}'
-
-
-# #     # Use re.sub() to replace matched patterns with an empty string
-#     cleaned_text = re.sub(pattern, '', first_paragraph)
-#     return cleaned_text
-
-# #
-# # @clean_par
-# def first_par(url):
-#     root_url = "https://country-leaders.onrender.com/cookie/"
-#     refresh_cookie(root_url)
-    
-#     # Make a request to the Wikipedia page
-#     r = requests.get(url)
-#     soup = BeautifulSoup(r.text, 'html.parser')
-
-#     # Find the specific div with the given id
-#     target_div = soup.find('div', id="mw-content-text")
-
-#     # Find all paragraphs within the target div
-#     paragraphs = target_div.find_all("p")
-
-#     for paragraph in paragraphs:
-#         # Check if the paragraph starts with a bold word
-#         if paragraph.find("b") and len(paragraph.get_text())>15:
-#             # Return the cleaned text or the original text
-#             cleaned_text = clean_par(paragraph.get_text())
-#             return cleaned_text
-
-
-
-
-
-
-# def to_json_file(leaders_data,filepath):
-#         with open(filepath, 'w') as file:
-#             json.dump(leaders_data, file)
-            
-            
-# # Set the initial time
-
-
-
-
-# #check statues
-# def get_country(url):
-#     root_url = url
-#     endpoint=["status/","cookie/","countries/"]
-
-#     for i in endpoint:
-#         response = s.get(f"{root_url}/{i}")
-#         #print(response.json())
-#         countries=response.json()
-#     return countries
-
-
-
-# root_url = "https://country-leaders.onrender.com"
-# countries=get_country(root_url)
-
-
-# dict = {}
-
-
-# for country in countries:
-    
-    
-#     print(country)
-#     get_country(root_url)
-    
-#     endpoint4 = "leaders?country="+country
-#     response = s.get(f"{root_url}/{endpoint4}")
-#     all_leaders=response.json()
-    
-    
-#     dict[country] = [{'first_name': leader["first_name"],"url":leader["wikipedia_url"],"first_paragraph":first_par(leader["wikipedia_url"])} for leader in all_leaders]
-
-#     print("post leader request")
-
-
-# to_json_file(dict,"leaders_data.json")
-
-# #print(dict["ma"][0]["url"])
-
-
-
-# Example usage:
 from src.scraper import WikipediaScraper
 
 def main():
@@ -121,6 +13,8 @@ def main():
 
     # Save the data to a JSON file
     scraper.to_json_file("leaders_data.json")
+    
+    
 
 if __name__ == "__main__":
     main()
--- src/scraper.py
+++ src/scraper.py
@@ -3,11 +3,62 @@ import json
 from bs4 import BeautifulSoup
 import re
 
+
 class WikipediaScraper:
-    def __init__(self):
+    def __init__(self) -> None:
         """
-        Initialize the WikipediaScraper object with necessary attributes
-        """
+    A class for scraping leader data from the Country Leaders API
+
+    Parameters
+    ----------
+    None
+
+    Attributes
+    ----------
+    root_url : str
+        The base URL of the Country Leaders API.
+
+    country_endpoint : str
+        The endpoint for retrieving the list of supported countries.
+
+    leaders_endpoint : str
+        The endpoint for retrieving the list of leaders for a specific country.
+
+    cookies_endpoint : str
+        The endpoint for refreshing the API cookie.
+
+    leaders_data : dict
+        A dictionary to store the scraped leader data.
+
+    session : requests.Session
+        A session object for making HTTP requests.
+
+    cookie : CookieJar or None
+        The cookie object used for API calls.
+
+    Methods
+    -------
+    refresh_cookie()
+        Refreshes the API cookie.
+
+    get_countries()
+        Retrieves the list of supported countries from the API.
+
+    get_leaders(country)
+        Retrieves the leaders of a specific country from the API.
+
+    clean_paragraph(first_paragraph)
+        Cleans the first paragraph by removing specific patterns.
+
+    get_first_paragraph(wikipedia_url)
+        Retrieves the first paragraph of details about a leader from their Wikipedia page.
+
+    to_json_file(filepath)
+        Stores the data structure into a JSON file.
+    """
+        
+        # Initialize the WikipediaScraper object with necessary attributes
+        
         self.root_url = "https://country-leaders.onrender.com"
         self.country_endpoint = "/countries"
         self.leaders_endpoint = "/leaders"
@@ -16,7 +67,10 @@ class WikipediaScraper:
         self.session = requests.Session()
         self.cookie = None
 
-    def refresh_cookie(self):
+    def refresh_cookie(self) -> None:
+        """
+        Refresh the API cookie and update the cookie attribute.
+        """
         try:
             response = self.session.get(self.root_url + self.cookies_endpoint)
             response.raise_for_status()
@@ -25,12 +79,14 @@ class WikipediaScraper:
             print(f"Error refreshing cookie: {e}")
             self.cookie = None  # Set to None to handle the error appropriately in your code
 
-    def get_countries(self):
+    def get_countries(self) -> list[str]:
         """
-        Retrieve the list of supported countries from the API
+        Retrieve the list of supported countries from the API.
 
-        Returns:
-        list: List of supported countries
+        Returns
+        -------
+        list
+            List of supported countries.
         """
         # Refresh the cookie before scraping each country
         self.refresh_cookie()
@@ -39,12 +95,14 @@ class WikipediaScraper:
         response = self.session.get(self.root_url + self.country_endpoint)
         return response.json()
 
-    def get_leaders(self, country):
+    def get_leaders(self, country: str) -> None:
         """
         Retrieve the leaders of a specific country from the API and populate the leaders_data object.
 
-        Args:
-        country (str): The name of the country.
+        Parameters
+        ----------
+        country : str
+            The name of the country.
         """
            # Refresh the cookie before scraping each country
         self.refresh_cookie()
@@ -68,30 +126,40 @@ class WikipediaScraper:
             } for leader in all_leaders
         ]
 
-    def clean_paragraph(self, first_paragraph):
+    def clean_paragraph(self, first_paragraph : str) -> str:
         """
         Clean the first paragraph by removing specific patterns.
 
-        Args:
-        first_paragraph (str): The original first paragraph.
+        Parameters
+        ----------
+        first_paragraph : str
+            The original first paragraph.
 
-        Returns:
-        str: The cleaned first paragraph.
+        Returns
+        -------
+        str
+            The cleaned first paragraph.
         """
         pattern = r'\([^)]*uitspraakⓘ[^)]*\)|\[\d+\]|\n' + f'|{r"//w+?;"}'
         cleaned_text = re.sub(pattern, '', first_paragraph)
         return cleaned_text
 
-    def get_first_paragraph(self, wikipedia_url):
+    def get_first_paragraph(self, wikipedia_url: str) -> str:
+        
             """
-            Get the first paragraph of details about a leader from their Wikipedia page
+            Get the first paragraph of details about a leader from their Wikipedia page.
 
-            Args:
-            wikipedia_url (str): URL of the leader's Wikipedia page
+            Parameters
+            ----------
+            wikipedia_url : str
+                URL of the leader's Wikipedia page.
 
-            Returns:
-            str: The cleaned first paragraph.
+            Returns
+            -------
+            str
+                The cleaned first paragraph.
             """
+        
             response = self.session.get(wikipedia_url)
             soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -114,13 +182,17 @@ class WikipediaScraper:
 
    
 
-    def to_json_file(self, filepath):
+    def to_json_file(self, filepath: str) -> None:
         """
-        Store the data structure into a JSON file
+        Store the data structure into a JSON file.
 
-        Args:
-        filepath (str): The path to the JSON file.
+        Parameters
+        ----------
+        filepath : str
+            The path to the JSON file.
         """
+        
+        
         with open(filepath, 'w',encoding= "utf-8") as file:
             json.dump(self.leaders_data, file, indent=2,ensure_ascii=False)
 
